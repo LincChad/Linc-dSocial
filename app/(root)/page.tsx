b68bd3eb28dcbec06f64479967f740df1bdac2ac
Linc Chad <linc@MacBook-Pro.local>
2023-09-13 14:08:55 +1000

    saving day 2

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+ 
+export default function Home() {
+  return (
+    <div>
+      <h1 className="head-text test-left">Home</h1>
+    </div>
+  )
+}
+
+
+{/* <UserButton afterSignOutUrl="/"/> */}
