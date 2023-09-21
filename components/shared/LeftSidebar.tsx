95af09dffd52f3b5058292e13bf140d1d4bfc602
Linc Chad <lincchad@protonmail.com>
2023-09-22 06:42:26 +1000

    Bug fix, added profile page & reply comments

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { sidebarLinks } from "@/constants";
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -8,6 +8,7 @@ import { usePathname, useRouter } from "next/navigation";
 function LeftSidebar() {
   const router = useRouter();
   const pathname = usePathname();
+  const { userId } = useAuth();
   return (
     <section className="leftsidebar custom-scrollbar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
@@ -15,6 +16,8 @@ function LeftSidebar() {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             link.route === pathname;
+
+            if(link.route === "/profile") link.route = `${link.route}/${userId}`
 
           return (
             <Link
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -145,3 +145,32 @@ export async function addCommentToThread(
     throw new Error(`Unable to add comment: ${error.message}`);
   }
 }
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB()
+
+    // Find all threads authored by user with the given id
+
+    // TODO: populate community
+    const threads = await User.findOne({ id: userId })
+      .populate({
+        path: 'threads',
+        model: Thread,
+        populate: {
+          path: 'children',
+          model: Thread,
+          populate: {
+            path: 'author',
+            model: User,
+            select: 'name image id'
+          }
+        }
+      })
+      return threads
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user posts: ${error.message}`);
+    
+  }
+  
+}
